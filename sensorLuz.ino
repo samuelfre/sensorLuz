37331a1316aa66dc9aef97ce37aa42c66c495800
samuelfre <samuel.hgs19@outlook.com>
2021-07-26 11:55:42 -0300

    first commit

---
+++ sensorLuz.ino
@@ -0,0 +1,12 @@
+const int pinoLDR = A0;
+int leitura = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
